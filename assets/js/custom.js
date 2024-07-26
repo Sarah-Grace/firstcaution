991f760aacaad3a624bb38fd86c0c4e10b1b2355
Sarah-Grace <sarah.grace4875@gmail.com>
2024-07-26 16:00:30 +0500

    created new pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,23 @@
-// $(".sidebar-menu-wrapper ul li a").hover(function() {
-//   $(".sidebar-menu-wrapper").addClass("mob-active-sidebar");
-// });
+$( document ).ready(function() {
+    $('#register').modal('show');
+});
+$( document ).ready(function() {
+    $('#login').modal('show');
+});
+let isShowpassword = false;
+const togglePassword = document
+        .querySelector('.togglePassword');
+    const password = document.querySelector('.myPassword');
+    togglePassword.addEventListener('click', () => {
+        // Toggle the type attribute using
+        // getAttribure() method
+        const type = password
+            .getAttribute('type') === 'password' ? 
+            'text' : 'password';
+            isShowpassword = type === 'password' ?
+            false : true;
+        password.setAttribute('type', type);
+        // Toggle the custom-fa-eye-slash class
+        isShowpassword ?
+        $(".togglePassword").addClass("custom-fa-eye-slash") : $(".togglePassword").removeClass("custom-fa-eye-slash");
+    });

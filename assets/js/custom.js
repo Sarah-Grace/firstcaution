97c95d5971dd4f7514496b2efd29fc22fffdca40
Sarah-Grace <sarah.grace4875@gmail.com>
2024-07-29 16:57:16 +0500

    worked on deposit pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,29 @@
-$( document ).ready(function() {
-    $('#register').modal('show');
-});
-$( document ).ready(function() {
-    $('#login').modal('show');
-});
+
+
 let isShowpassword = false;
-const togglePassword = document
-        .querySelector('.togglePassword');
-    const password = document.querySelector('.myPassword');
-    togglePassword.addEventListener('click', () => {
-        // Toggle the type attribute using
-        // getAttribure() method
-        const type = password
-            .getAttribute('type') === 'password' ? 
-            'text' : 'password';
-            isShowpassword = type === 'password' ?
-            false : true;
-        password.setAttribute('type', type);
-        // Toggle the custom-fa-eye-slash class
-        isShowpassword ?
-        $(".togglePassword").addClass("custom-fa-eye-slash") : $(".togglePassword").removeClass("custom-fa-eye-slash");
-    });
+const password = document.querySelector('.myPassword');
+
+$('.togglePassword').click(
+    () => {
+    // Toggle the type attribute using
+    // getAttribure() method
+    const type = password
+        .getAttribute('type') === 'password' ? 
+        'text' : 'password';
+        isShowpassword = type === 'password' ?
+        false : true;
+    password.setAttribute('type', type);
+    // Toggle the custom-fa-eye-slash class
+    isShowpassword ?
+    $(".togglePassword").addClass("custom-fa-eye-slash") : $(".togglePassword").removeClass("custom-fa-eye-slash");
+}
+);
+
+// calcution flexible amount 
+$(document).on('input', '#flexible-amount', function() {
+    $('#flexible-amount-field').val( $(this).val() );
+});
+
+$("#calcPremium").click(function(){
+    $(".adjust-detail-transfer-deposit-section").show()
+});
